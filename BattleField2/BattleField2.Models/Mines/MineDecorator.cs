c46db851b2bc6be86fb9b0d298e80884b97079a2
vyara <vyara_hristova@yahoo.de>
2015-10-01 12:06:39 +0300

    created a MineDecorator.cs (#3)

---
+++ BattleField2/BattleField2.Models/Mines/MineDecorator.cs
@@ -0,0 +1,22 @@
+﻿
+namespace BattleField2.Models.Mines
+{
+    using Coordinates;
+    using Cells;
+    using System;
+
+    internal abstract class MineDecorator : Explosive
+    {
+        protected MineDecorator(Explosive explosive)
+        {
+            this.Explosive = explosive;
+        }
+
+        protected Explosive Explosive { get; private set; }
+
+        public override Cell[,] Detonate(int currentFieldSize, Cell[,] fieldPositions)
+        {
+            this.Explosive.Detonate(currentFieldSize, fieldPositions);
+        }
+    }
+}
