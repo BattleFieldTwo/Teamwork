48da0a7596ba545613990e989cfa595295fa6f20
Nikola Hristov <anarhisthristov@gmail.com>
2015-10-03 19:27:46 +0300

    MEGA REFACTORING

---
+++ BattleField2/BattleField2.GameLogic/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿namespace BattleField2.GameLogic
+{
+    using BattleField2.Renderers;
+
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            IGameRenderer consoleRenderer = ConsoleGameRenderer.Instance;
+            GameEngine gameEngine = GameEngine.Instance(consoleRenderer);
+
+            gameEngine.InitializeGame();
+            gameEngine.PlayGame();
+        }
+    }
+}
---
+++ BattleField2/BattleField2.ViewModels/IGameRenderer.cs
@@ -0,0 +1,16 @@
+﻿namespace BattleField2.Renderers
+{
+    using BattleField2.Models.Field;
+    using BattleField2.Models.Coordinates;
+
+    public interface IGameRenderer
+    {
+        void DisplayMessage(string message);
+
+        void DrawField(Field currentBattleField);
+
+        string EnterCommand();
+
+        void Wait();
+    }
+}
