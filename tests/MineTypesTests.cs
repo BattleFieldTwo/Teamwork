6b894d36c74e3cc8fd9976efbec66f91ddec449e
vyara <vyara_hristova@yahoo.de>
2015-10-10 23:49:51 +0300

    added Mine Tests - Models.Mines now with 98.3% code coverage

--- tests/MineTypesTests.cs
+++ tests/MineTypesTests.cs
@@ -6,6 +6,7 @@
     using Models.Cells;
     using Models.Coordinates;
     using Moq;
+    using System.Collections.Generic;
 
     [TestClass]
     public class MineTypesTests
@@ -19,6 +20,74 @@
             var mineMock = new Mock<IExplosive> (); 
             mineMock.Setup(m => m.Detonate(It.IsAny<Cell[,]>(), It.IsAny<Coordinates>())).Returns(testCell);
             Assert.AreEqual(mineMock.Object.Detonate(fieldPositions, currentCoordinates), testCell);
+        }
+
+        [TestMethod]
+        public void ValidateThatMineDetonateMethodFunctions()
+        {
+            var fieldPositions = new Cell[3, 3];
+            var currentCoordinates = new Coordinates(2, 2);
+            var testCoordinates = new List<Coordinates>()
+            {
+
+            };
+            var testCell = new Cell[1, 2];
+            var baseMineMock = new Mock<Mine>();
+            baseMineMock.Setup(b => b.Detonate(It.IsAny<Cell[,]>(), It.IsAny<Coordinates>())).Verifiable();
+
+            var mineMockObject = baseMineMock.Object;
+            mineMockObject.Detonate(fieldPositions, currentCoordinates);
+            baseMineMock.VerifyAll();
+        }
+
+        [TestMethod]
+        public void ValidateThatMinеDetonateMethodFunctions_LevelOne()
+        {
+            var fieldPositions = new Cell[3, 3];
+            var currentCoordinates = new Coordinates(2, 2);
+            var testCell = new Cell[1, 2];
+            var minineMock = new MineLevelOneUpgrade();
+            Assert.AreEqual(minineMock.Detonate(fieldPositions, currentCoordinates), fieldPositions);
+        }
+
+        [TestMethod]
+        public void ValidateThatMinеDetonateMethodFunctions_LevelTwo()
+        {
+            var fieldPositions = new Cell[3, 3];
+            var currentCoordinates = new Coordinates(2, 2);
+            var testCell = new Cell[1, 2];
+            var minineMock = new MineLevelTwoUpgrade();
+            Assert.AreEqual(minineMock.Detonate(fieldPositions, currentCoordinates), fieldPositions);
+        }
+
+        [TestMethod]
+        public void ValidateThatMinеDetonateMethodFunctions_LevelThree()
+        {
+            var fieldPositions = new Cell[3, 3];
+            var currentCoordinates = new Coordinates(2, 2);
+            var testCell = new Cell[1, 2];
+            var minineMock = new MineLevelThreeUpgrade();
+            Assert.AreEqual(minineMock.Detonate(fieldPositions, currentCoordinates), fieldPositions);
+        }
+
+        [TestMethod]
+        public void ValidateThatMinеDetonateMethodFunctions_LevelFour()
+        {
+            var fieldPositions = new Cell[3, 3];
+            var currentCoordinates = new Coordinates(2, 2);
+            var testCell = new Cell[1, 2];
+            var minineMock = new MineLevelFourUpgrade();
+            Assert.AreEqual(minineMock.Detonate(fieldPositions, currentCoordinates), fieldPositions);
+        }
+
+        [TestMethod]
+        public void ValidateThatMinеDetonateMethodFunctions_LevelFive()
+        {
+            var fieldPositions = new Cell[3, 3];
+            var currentCoordinates = new Coordinates(2, 2);
+            var testCell = new Cell[1, 2];
+            var minineMock = new MineLevelFiveUpgrade();
+            Assert.AreEqual(minineMock.Detonate(fieldPositions, currentCoordinates), fieldPositions);
         }
 
         [TestMethod]
