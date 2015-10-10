fb0e5d6b3669ea9a335a0a74996dbd4444377caf
vyara <vyara_hristova@yahoo.de>
2015-10-10 22:06:05 +0300

    added MineLevels and MineFactory tests

--- tests/MineTypesTests.cs
+++ tests/MineTypesTests.cs
@@ -11,37 +11,58 @@
     public class MineTypesTests
     {
         [TestMethod]
-        public void CheckIfCreatingAMineWithALevelUpgradeWorksAsExpected_LevelOne()
+        public void CheckIfIExplosiveDetonateMethodReturnsProperResult()
         {
             var fieldPositions = new Cell[4, 4];
-            var currentCellFactory = CellFactory.Instance();
+            //var currentCellFactory = CellFactory.Instance();
             // These coordinates should be the position of the mine
             var currentCoordinates = new Coordinates(2, 2);
             var testCell = new Cell[1, 2];
-            var mineMock = new Mock<IExplosive> (); //basic Mine class 
+            var mineMock = new Mock<IExplosive> (); 
             //mineMock = new MineLevelOneUpgrade(); //Decorated Mine object
             mineMock.Setup(m => m.Detonate(fieldPositions, currentCoordinates)).Returns(testCell);
             Assert.AreEqual(mineMock.Object.Detonate(fieldPositions, currentCoordinates), testCell);
         }
 
         [TestMethod]
-        public void CheckIfCreatingAMineWithALevelUpgradeWorksAsExpected_LevelTwo()
+        public void CheckIfStringRepresentationMethodWorksReturnsAProperValue_MineLevelOneUpgrade()
         {
+            var mineLevelOne = new MineLevelOneUpgrade();
+
+            Assert.AreEqual(mineLevelOne.StringRepresentation, " 1 ");
         }
 
         [TestMethod]
-        public void CheckIfCreatingAMineWithALevelUpgradeWorksAsExpected_LevelThree()
+        public void CheckIfStringRepresentationMethodWorksReturnsAProperValue_MineLevelTwoUpgrade()
         {
+            var mineLevelOne = new MineLevelTwoUpgrade();
+
+            Assert.AreEqual(mineLevelOne.StringRepresentation, " 2 ");
         }
 
         [TestMethod]
-        public void CheckIfCreatingAMineWithALevelUpgradeWorksAsExpected_LevelFour()
+        public void CheckIfStringRepresentationMethodWorksReturnsAProperValue_MineLevelThreeUpgrade()
         {
+            var mineLevelOne = new MineLevelThreeUpgrade();
+
+            Assert.AreEqual(mineLevelOne.StringRepresentation, " 3 ");
         }
 
         [TestMethod]
-        public void CheckIfCreatingAMineWithALevelUpgradeWorksAsExpected_LevelFive()
+        public void CheckIfStringRepresentationMethodWorksReturnsAProperValue_MineLevelFourUpgrade()
         {
+            var mineLevelOne = new MineLevelFourUpgrade();
+
+            Assert.AreEqual(mineLevelOne.StringRepresentation, " 4 ");
+        }
+
+
+        [TestMethod]
+        public void CheckIfStringRepresentationMethodWorksReturnsAProperValue_MineLevelFiveUpgrade()
+        {
+            var mineLevelOne = new MineLevelFiveUpgrade();
+
+            Assert.AreEqual(mineLevelOne.StringRepresentation, " 5 ");
         }
     }
 }
