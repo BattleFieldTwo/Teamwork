ccd3eb835837e794764543208435363bfd6a2711
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2015-10-12 00:18:26 +0300

    Added mines documentation comments

--- src/BattleField2.Models/Mines/IExplosive.cs
+++ src/BattleField2.Models/Mines/IExplosive.cs
@@ -2,9 +2,17 @@
 {
     using Cells;
     using Coordinates;
-
+    /// <summary>
+    /// Interface IExplosive to be implemented by a concrete Explosive class.
+    /// </summary>
     public interface IExplosive
     {
+        /// <summary>
+        /// Method that should implement a detonation logic for the mines.
+        /// </summary>
+        /// <param name="fieldPositions">Detonation position.</param>
+        /// <param name="currentCoordinates">Coordinates on the field.</param>
+        /// <returns></returns>
         Cell[,] Detonate(Cell[,] fieldPositions, Coordinates currentCoordinates);
     }
 }
