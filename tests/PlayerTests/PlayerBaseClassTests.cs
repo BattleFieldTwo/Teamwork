cce8810f10ac7eceed093084ff5ac41754081f2b
vyara <vyara_hristova@yahoo.de>
2015-10-11 18:16:37 +0300

    added some player tests/BattleField2.Tests.csproj

---
+++ tests/PlayerTests/PlayerBaseClassTests.cs
@@ -0,0 +1,47 @@
+﻿namespace BattleField2.Tests.PlayerTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Models.Player;
+
+    [TestClass]
+    public class PlayerBaseClassTests
+    {
+        [TestMethod]
+        public void VerifyThatPlayerNameIsSetToNullIfInvalidDataIsProvided_EmptyString()
+        {
+            var player = new Player("");
+
+            Assert.IsNull(player.Name);
+        }
+
+        [TestMethod]
+        public void VerifyThatPlayerNameIsSetToNullIfInvalidDataIsProvided_InvalidStringSymbols()
+        {
+            var player = new Player("$#7jahs @");
+
+            Assert.IsNull(player.Name);
+        }
+
+        [TestMethod]
+        public void VerifyThatPlayerNameIsSetCorrectlyIfValidDataIsProvided()
+        {
+            var name = "JohnDoe";
+            var player = new Player(name);
+
+            Assert.AreEqual(player.Name, name);
+        }
+
+        [TestMethod]
+        public void VerifyThatPlayerScoreIsSetTZeroIfInvalidDataIsProvided_NegativeNumber()
+        {
+            var name = "JohnDoe";
+            var negativeScore = -1;
+            var player = new Player(name);
+            player.Score = negativeScore;
+
+
+            Assert.AreEqual(player.Score, 0);
+        }
+    }
+}
