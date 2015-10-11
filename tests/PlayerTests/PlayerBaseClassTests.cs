63c9ff5510b34d583ba0bb9647d39d20f3d641f1
vyara <vyara_hristova@yahoo.de>
2015-10-11 18:30:21 +0300

    player.cs is fully covered

--- tests/PlayerTests/PlayerBaseClassTests.cs
+++ tests/PlayerTests/PlayerBaseClassTests.cs
@@ -31,6 +31,13 @@
 
             Assert.AreEqual(player.Name, name);
         }
+        [TestMethod]
+        public void VerifyThatPlayerNameIsSetToNullIfEmptyConstructorIsUsed()
+        {
+            var player = new Player();
+
+            Assert.IsNull(player.Name);
+        }
 
         [TestMethod]
         public void VerifyThatPlayerScoreIsSetTZeroIfInvalidDataIsProvided_NegativeNumber()
@@ -42,6 +49,63 @@
 
 
             Assert.AreEqual(player.Score, 0);
+        }
+
+        [TestMethod]
+        public void VerifyThatPlayerScoreIsSetCorrectlyIfValidDataIsProvided_Zero()
+        {
+            var name = "JohnDoe";
+            var zeroScore = 0;
+            var player = new Player(name);
+            player.Score = zeroScore;
+
+
+            Assert.AreEqual(player.Score, zeroScore);
+        }
+
+        [TestMethod]
+        public void VerifyThatPlayerScoreIsSetCorrectlyIfValidDataIsProvided_ValidScoreNotZero()
+        {
+            var name = "JohnDoe";
+            var validScore = 25;
+            var player = new Player(name);
+            player.Score = validScore;
+
+
+            Assert.AreEqual(player.Score, validScore);
+        }
+
+        [TestMethod]
+        public void VerifyThatCalculateScoreMethodReturnsZeroIfMineDifferenceIsOne()
+        {
+
+           var player = new Player();
+
+            var initialMinesCount = 3;
+            var finalMinesCount = 2;
+
+            var playerScore = player.CalculateScore(initialMinesCount, finalMinesCount);
+
+            Assert.AreEqual(playerScore, 1);
+   
+        }
+
+        [TestMethod]
+        public void VerifyThatCalculateScoreMethodReturnsProperValueIfMinesDifferenceIsMoreThanOne()
+        {
+
+            var player = new Player();
+
+            var initialMinesCount = 5;
+            var finalMinesCount = 2;
+            var differenceInMinesDetonated = initialMinesCount - finalMinesCount;
+
+            var calculatedScore = 1 + (differenceInMinesDetonated - 1) * 2;
+
+            var playerScore = player.CalculateScore(initialMinesCount, finalMinesCount);
+
+            Assert.AreEqual(playerScore, calculatedScore);
+
         }
     }
 }
