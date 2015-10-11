74decca1328733d1e21f6cfa5479df66ea8a9329
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2015-10-11 23:52:38 +0300

    Added IViewModel comments

--- src/BattleField2.Renderers/Contracts/IViewModel.cs
+++ src/BattleField2.Renderers/Contracts/IViewModel.cs
@@ -3,51 +3,51 @@
     using BattleField2.Models.Field;
     using BattleField2.Models.Coordinates;
     /// <summary>
-    /// 
+    /// IViewModel interface to be inherited by a concrete ViewModel.
     /// </summary>
     public interface IViewModel
     {
         /// <summary>
-        /// 
+        /// Method to display the in-game messages.
         /// </summary>
-        /// <param name="welcomeMessage"></param>
+        /// <param name="welcomeMessage">Message to be handled.</param>
         void DisplayWelcomeMessage(string welcomeMessage);
 
         /// <summary>
-        /// 
+        /// Method to get the field size.
         /// </summary>
         /// <param name="inviteToGiveSizeMessage"></param>
-        /// <returns></returns>
+        /// <returns>Returns field size.</returns>
         int GetFieldSize(string inviteToGiveSizeMessage);
 
         /// <summary>
-        /// 
+        /// Method to draw the game field.
         /// </summary>
-        /// <param name="currentBattleField"></param>
+        /// <param name="currentBattleField">Field object to be drawn.</param>
         void DrawField(Field currentBattleField);
 
         /// <summary>
-        /// 
+        /// Method to get input coordinates.
         /// </summary>
         /// <param name="inviteToEnterCoordinatesMessage"></param>
-        /// <returns></returns>
+        /// <returns>Returns the new coordinates.</returns>
         Coordinates GetInputCoordinates(string inviteToEnterCoordinatesMessage);
 
         /// <summary>
-        /// 
+        /// Method that notifies player on invalid move.
         /// </summary>
-        /// <param name="invalidMoveNotificationMessage"></param>
+        /// <param name="invalidMoveNotificationMessage">Message to notify for invalid string.</param>
         void NotifyForInvalidMove(string invalidMoveNotificationMessage);
 
         /// <summary>
-        /// 
+        /// Method that gets called at the end of the game.
         /// </summary>
         /// <param name="gameOverMessage"></param>
         /// <param name="detonatedMines"></param>
         void GameOver(string gameOverMessage, int detonatedMines);
 
         /// <summary>
-        /// 
+        /// Method that gives the mines count.
         /// </summary>
         /// <param name="count"></param>
         void GiveMinesCount(int count);
