9886bfc945422d0d251992a60ad8d723780cc7e8
vyara <vyara_hristova@yahoo.de>
2015-10-11 13:27:05 +0300

    added some Field Method tests

--- tests/FieldTests/FieldTests.cs
+++ tests/FieldTests/FieldTests.cs
@@ -4,15 +4,70 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
     using Models.Field;
+    using Models.Coordinates;
+
     //using Telerik.JustMock;
 
     [TestClass]
-    public class FieldTests 
+    public class FieldTests
     {
         [TestMethod]
         public void CheckIfGenerateFieldMethodIsBeingCalledProperly()
         {
+            var fieldMock = new Mock<IField>();
+
+            fieldMock.CallBase = true;
+            fieldMock.Setup(f => f.GenerateField()).Verifiable();
+
+            fieldMock.Object.GenerateField();
+
+            fieldMock.VerifyAll();
 
         }
+
+
+        [TestMethod]
+        public void CheckIfPositionMinesMethodIsBeingCalledProperly()
+        {
+            var fieldMock = new Mock<IField>();
+
+            fieldMock.CallBase = true;
+            fieldMock.Setup(f => f.PositionMines()).Verifiable();
+
+            fieldMock.Object.PositionMines();
+
+            fieldMock.VerifyAll();
+
+        }
+
+        [TestMethod]
+        public void CheckIfCountRemainingMinesMethodIsBeingCalledProperly()
+        {
+            var fieldMock = new Mock<IField>();
+
+            fieldMock.CallBase = true;
+            fieldMock.Setup(f => f.CountRemainingMines()).Returns(5);
+
+
+            Assert.AreEqual(fieldMock.Object.CountRemainingMines(), 5);
+
+        }
+
+
+        [TestMethod]
+        public void CheckIfValidateMoveCoordinatesMethodIsBeingCalledProperly()
+        {
+            var fieldMock = new Mock<IField>();
+            var testCoordinates = new Coordinates(2, 3);
+
+            fieldMock.CallBase = true;
+            fieldMock.Setup(f => f.ValidateMoveCoordinates(testCoordinates)).Returns(true);
+
+
+            Assert.IsTrue(fieldMock.Object.ValidateMoveCoordinates(testCoordinates));
+
+        }
+
+
     }
 }
