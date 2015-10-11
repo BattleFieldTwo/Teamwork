b686ab25882a433c3023e77b8531302c00d8e215
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2015-10-11 22:32:16 +0300

    Added documenting comments for the Cell classes.

--- src/BattleField2.Models/Cells/CellType.cs
+++ src/BattleField2.Models/Cells/CellType.cs
@@ -1,8 +1,18 @@
 ﻿namespace BattleField2.Models.Cells
 {
+    /// <summary>
+    /// Enumeration that holds the two basic CellType types.
+    /// The Empty cell type and the Detonated cell type.
+    /// </summary>
     public enum CellType
     {
+        /// <summary>
+        /// Enumeration value representing the Empty cell type.
+        /// </summary>
         Empty,
+        /// <summary>
+        /// Enumeration value representing the Detonated cell type.
+        /// </summary>
         Detonated
     }
 }
