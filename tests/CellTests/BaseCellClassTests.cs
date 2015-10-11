d8859b561c88585fcb3377d21868f337c8211f37
vyara <vyara_hristova@yahoo.de>
2015-10-11 11:56:54 +0300

    started Cells unit testing

---
+++ tests/CellTests/BaseCellClassTests.cs
@@ -0,0 +1,23 @@
+﻿namespace BattleField2.Tests.CellTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using BattleField2.Models.Cells;
+
+
+    [TestClass]
+    public class CellClassTests
+    {
+        [TestMethod]
+        public void CheckIfStringRepresentationMethodFunctions()
+        {
+            var baseCellmock = new Mock<Cell>();
+            baseCellmock.Setup(b => b.StringRepresentation).Returns("test");
+            var baceCellMockObject = baseCellmock.Object;
+
+            Assert.AreEqual(baceCellMockObject.StringRepresentation, "test");
+
+        }
+    }
+}
